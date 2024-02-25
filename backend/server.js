4bea1c6623ed0a2a94aa9a13aa85d6defb90dafd
dusanpracaex <161168505+dusanpracaex@users.noreply.github.com>
2024-02-25 17:05:25 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); //Pass json data
 // corse middleware
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://matias-kiam.onrender.com"],
   credentials: true,
 };
 app.use(corse(corsOptions));
