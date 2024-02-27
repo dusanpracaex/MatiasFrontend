e22b0bcc61d91c051c3b55d1ae837aba8471daaa
dusanpracaex <161168505+dusanpracaex@users.noreply.github.com>
2024-02-28 05:05:10 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,6 @@ app.use(express.json()); //Pass json data
 // corse middleware
 const corsOptions = {  
   origin: ['https://goldfish-app-gwh4l.ondigitalocean.app/'],
-  optionsSuccessStatus:200,
   credentials: true};
 app.use(cors(corsOptions));
 // Passport middleware
