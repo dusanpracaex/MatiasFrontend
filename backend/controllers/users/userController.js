000f0a27cd1b783369412538d955df7c98fc6896
dusanpracaex <161168505+dusanpracaex@users.noreply.github.com>
2024-02-28 21:54:38 +0800

    Update userController.js

--- backend/controllers/users/userController.js
+++ backend/controllers/users/userController.js
@@ -85,7 +85,7 @@ const userController = {
         res.cookie("token", token, {
           httpOnly: true,
           secure: false,
-          sameSite: "strict",
+          sameSite: "None",
           maxAge: 24 * 60 * 60 * 1000, //1 day:
         });
         //redirect the user dashboard
